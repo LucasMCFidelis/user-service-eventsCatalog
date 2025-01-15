873d7a74b684d73d68f1b3381fe3e0d8dd8f19bb
Lucas Fidelis <lucasm241301@gmail.com>
2025-01-14 22:56:22 -0300

    Adiciona autenticação e autorização nas rotas onde é necessario

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,36 @@
 import { FastifyInstance } from "fastify";
-import { createUserRoute, deleteUserRoute, getUserByIdRoute, updateUserPasswordRoute, updateUserRoute, validateUserCredentialsRoute} from "../controllers/userController.js";
+import {
+  createUserRoute,
+  deleteUserRoute,
+  getUserByIdRoute,
+  updateUserPasswordRoute,
+  updateUserRoute,
+  validateUserCredentialsRoute,
+} from "../controllers/userController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { authorizeUserById } from "../middlewares/authorizeUserById.js";
+import { CadastreUser } from "../interfaces/cadastreUserInterface.js";
 
 export async function userRoutes(server: FastifyInstance) {
-    server.post("/", createUserRoute);     // POST /usuarios
-    server.get("/:id", getUserByIdRoute);     // GET /usuarios/{id}
-    server.delete<{ Params: { id: string } }>("/:id", { preHandler: [authMiddleware, authorizeUserById] },deleteUserRoute);     // DELETE /usuarios/{id}
-    server.put("/:id", updateUserRoute);     // PUT /usuarios/{id}
-    server.patch("/recuperacao/atualizar-senha", updateUserPasswordRoute);     // PATCH usuarios/recuperacao/atualizar-senha
-    server.post('/validate-credentials', validateUserCredentialsRoute);     // POST /usuarios/validate-credentials
+  server.post("/", createUserRoute); // POST /usuarios
+  server.get<{ Params: { id: string } }>(
+    "/:id",
+    { preHandler: [authMiddleware, authorizeUserById] },
+    getUserByIdRoute
+  ); // GET /usuarios/{id}
+  server.delete<{ Params: { id: string } }>(
+    "/:id",
+    { preHandler: [authMiddleware, authorizeUserById] },
+    deleteUserRoute
+  ); // DELETE /usuarios/{id}
+  server.put<{
+    Params: { id: string };
+    Body: Partial<CadastreUser>;
+  }>(
+    "/:id",
+    { preHandler: [authMiddleware, authorizeUserById] },
+    updateUserRoute
+  ); // PUT /usuarios/{id}
+  server.patch("/recuperacao/atualizar-senha", updateUserPasswordRoute); // PATCH usuarios/recuperacao/atualizar-senha
+  server.post("/validate-credentials", validateUserCredentialsRoute); // POST /usuarios/validate-credentials
 }
