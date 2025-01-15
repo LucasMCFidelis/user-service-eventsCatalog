0bf96084eb29603c7ede208083725bb8d98d0e36
Lucas Fidelis <lucasm241301@gmail.com>
2025-01-14 22:32:44 -0300

    Ajusta Middleware authorizeUserById para ser executado corretamente após a autenticação do usuário

--- src/middlewares/authorizeUserById.ts
+++ src/middlewares/authorizeUserById.ts
@@ -1,17 +1,20 @@
-import { FastifyReply, FastifyRequest } from 'fastify'
-import { ErrorResponse } from '../types/errorResponseType.js'
+import { FastifyReply, FastifyRequest } from "fastify";
+import { ErrorResponse } from "../types/errorResponseType.js";
 
-export function authorizeUserById(targetUserId: string) {
-    return (request: FastifyRequest, reply: FastifyReply) => {       
-        const { user } = request
-        
-        // Verifica se o usuário logado é o mesmo que o alvo da operação
-        if (!user || targetUserId !== user.userId) {
-            const errorValue: ErrorResponse = 'Erro de autorização'
-            return reply.status(403).send({ 
-                error: errorValue,
-                message: 'Acesso negado. O ID do usuário logado não corresponde ao ID solicitado para a operação.' 
-            })
-        }
-    }
+export async function authorizeUserById(
+  request: FastifyRequest<{ Params: { id: string } }>,
+  reply: FastifyReply
+) {
+  const { user } = request;
+  const targetUserId = request.params.id; // Supõe que o ID alvo está nos parâmetros da rota
+
+  // Verifica se o usuário logado é o mesmo que o alvo da operação
+  if (!user || targetUserId !== user.userId) {
+    const errorValue: ErrorResponse = "Erro de autorização";
+    return reply.status(403).send({
+      error: errorValue,
+      message:
+        "Acesso negado. O ID do usuário logado não corresponde ao ID solicitado para a operação.",
+    });
+  }
 }
