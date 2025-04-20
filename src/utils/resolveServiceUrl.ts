bb552b238e14062575f1c2e7dc6e31f3242a14b7
Lucas Fidelis <lucasm241301@gmail.com>
2025-04-19 21:19:27 -0300

    Adiciona aviso para caso uma variavel buscada com resolveServiceUrl não seja encontrada no ambiente

--- src/utils/resolveServiceUrl.ts
+++ src/utils/resolveServiceUrl.ts
@@ -1,5 +1,15 @@
 export function resolveServiceUrl(serviceName: string) {
-  const environment = process.env.NODE_ENV || "development";
+  const environment = process.env.NODE_ENV;
+
   const suffix = environment === "production" ? "PROD" : "DEV";
-  return process.env[`${serviceName}_SERVICE_URL_${suffix}`];
+  const key = `${serviceName}_SERVICE_URL_${suffix}`;
+  const url = process.env[key];
+
+  if (!url) {
+    console.error(
+      `A variável de ambiente "${key}" não está definida. Verifique se ela está presente no ambiente atual (NODE_ENV="${environment}").`
+    );
+  }
+
+  return url;
 }
